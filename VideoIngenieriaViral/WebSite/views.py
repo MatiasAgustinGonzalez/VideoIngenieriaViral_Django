1c579fd8950c14ec2c22bf3a06c9d01a102a1b69
Matias Gonzalez <matigonzalez93@hotmail.com.ar>
2024-10-12 11:03:04 -0300

    V 0.0.0 | Creación Estructura Django

---
+++ VideoIngenieriaViral/WebSite/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+
+
+def home_view(request):
+    return render(request, "WebSite/home.html")
